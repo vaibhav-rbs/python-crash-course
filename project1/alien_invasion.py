cac2258b994ed3e38ac9407ba2738bbc31ed3c5b
vaibhav-rbs <vaibhav.rbs@gmail.com>
2016-08-22 00:28:01 -0700

    on page288

--- project1/alien_invasion.py
+++ project1/alien_invasion.py
@@ -1,20 +1,23 @@
-import sys
-
 import pygame
+from settings import Settings
+from ship import Ship
+import game_functions as gf
+
 
 def run_game():
 	pygame.init()
-	screen = pygame.display.set_mode((1200,800))
+	ai_settings = Settings()
+	screen = pygame.display.set_mode(
+		(ai_settings.screen_width, ai_settings.screen_height))
 	pygame.display.set_caption("Alien Invasion")
-	bg_color = (230,230,230)
+
+	# Make a ship
+	ship = Ship(ai_settings,screen)
 
 	while True:
+		gf.check_events(ship)
+		ship.update()
+		gf.update_screen(ai_settings,screen,ship)
 
-		for event in pygame.event.get():
-			screen.fill(bg_color)
-			if event.type == pygame.QUIT:
-				sys.exit()
-
-	pygame.display.flip()
 
 run_game()
---
+++ project1/settings.py
@@ -0,0 +1,12 @@
+class Settings():
+	"""A class to store all settings for Alien Invasion"""
+
+	def __init__(self):
+		"""Initialize the game's settings"""
+		# screen settings
+		self.screen_width = 640
+		self.screen_height = 480
+		self.bg_color = (200,20,210)
+		# Ship settings
+		self.ship_speed_factor = 1.5
+	
