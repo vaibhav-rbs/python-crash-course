34f9c0ee344efe8b6fddd49ad4da918b4a396230
vaibhav-rbs <vaibhav.rbs@gmail.com>
2016-08-24 12:08:53 -0700

    Added bullet settings

--- project1/settings.py
+++ project1/settings.py
@@ -9,4 +9,10 @@ class Settings():
 		self.bg_color = (200,20,210)
 		# Ship settings
 		self.ship_speed_factor = 1.5
+		# Bullet Settings
+		self.bullet_speed_factor = 1
+		self.bullet_width = 3
+		self.bullet_height = 15
+		self.bullet_color = 60,60,60
+
 	
